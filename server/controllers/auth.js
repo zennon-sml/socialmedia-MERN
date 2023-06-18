91ab2d8943e894422f2ba48dc7e4a51742524a71
zennon-sml <zennonsampaioml@gmail.com>
2023-06-18 13:20:55 -0300

    feat: login done, returning jwt signed

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -34,6 +34,30 @@ export const register = async (req, res) => {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
     } catch (err ){
-        res.status(500).json({ error: err.message });
+        res.status(500).json({ 
+            msg:"could not register",
+             error: err.message 
+        });
+    }
+}
+
+/* LOGIN */
+export const login = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+        const user = await User.findOne({ email: email});
+        if (!user) return res.status(400).json({ msg: "User is does not exist."})
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials."})
+
+        const token = jwt.sign({id : user._id }, process.env.JWT_SECRET);
+        delete user.password;
+        res.status(200).json({ token, user });
+    } catch (err) {
+        res.status(500).json({
+            msg: "could not log in",
+            error: err.message
+        })
     }
 }
