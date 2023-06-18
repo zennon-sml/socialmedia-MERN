77a3e9fef07a985b1a3fa799ccdefddca15ce2ca
zennon-sml <zennonsampaioml@gmail.com>
2023-06-18 10:49:06 -0300

    feat: user registerto DB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import Express, { application } from "express"; 
+import express, { application } from "express"; 
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
@@ -8,7 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-import { Console } from "console";
+import { register } from "../server/controllers/auth.js"
 
 /* CONFIGURATIONs */
 
@@ -22,7 +22,7 @@ app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.user(cors());
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 /* FILE STORAGE */
@@ -35,8 +35,13 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({storage})
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL, {});
-    .then(() => app.listen(PORT, () => console.log()))
+mongoose.connect(process.env.MONGO_URL, {})
+    .then(() => {
+        app.listen(PORT, () => console.log(`server PORT: ${PORT}`))
+    })
+    .catch((error) => console.log(`Did Not Conect Error: ${error}`))
